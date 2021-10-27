540358c0edd6e01babcc8ff7627e08bc7788a776
Axel <axosar@gmail.com>
2021-10-26 20:10:59 -0500

    carousel item

---
+++ pages/components/ItemCarousel/ItemCarousel.js
@@ -0,0 +1,94 @@
+import ItemsCarousel from 'react-items-carousel';
+
+export default function ItemCarousel() {
+  return (
+    <>
+    <div style={{ "padding": "0 0 15px 15px", "maxWidth": "100%", "margin": "20px 0" }}>
+      <ItemsCarousel
+        infiniteLoop={true}
+        gutter={12}
+        activePosition={'center'}
+        chevronWidth={60}
+        disableSwipe={false}
+        alwaysShowChevrons={false}
+        numberOfCards={4}
+        slidesToScroll={1}
+        outsideChevron={false}
+        showSlither={false}
+        firstAndLastGutter={true}
+        activeItemIndex={0}
+        requestToChangeActive={1}
+        rightChevron={<i className="lni-angle-double-right"></i>}
+        leftChevron={<i className="lni-angle-double-left"></i>}
+      >
+        {Array.from(new Array(10)).map((_, i) =>
+          <div
+            key={i}
+            className="ItemCarousel"
+          >
+            <div>
+              <img src="/logo.svg" alt="Logo" style={{ width: '100px' }} />
+            </div>
+            <div>
+              <h2 onClick={() => this.props.toggleCarousel('prev')}>HOLA</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div>
+              <i className="lni-plus" onClick={() => this.props.toggleCarousel('prev')}></i>
+            </div>
+          </div>
+        )}
+      </ItemsCarousel>
+    </div>
+    <style jsx>{`
+      .ItemCarousel {
+        background: rgba(0,0,0,0.3);
+        padding: 10px;
+        display: flex;
+        flex-direction: row;
+        transition: all 1s;
+        justify-content: center;
+        align-content: center;
+        align-items: center;
+      }
+      
+      .ItemCarousel:hover {
+        background: rgba(0,0,0,0.8);
+      }
+      
+      i {
+        color: #ffffff;
+        font-size: 1.3em;
+      }
+      
+      .ItemCarousel h2 {
+        font-size: 1.3em;
+        text-align: left;
+        padding-left: 10px;
+        cursor: pointer;
+        padding-bottom: 0;
+        margin-bottom: 0;
+        margin-top: 0;
+        padding-top: 0;
+      }
+      
+      .ItemCarousel p {
+        font-size: 0.7em;
+        color: #ffffff;
+        text-align:left;
+        padding-left: 10px;
+      }
+      
+      .ItemCarousel i.lni-plus {
+        cursor: pointer;
+        font-size:1.5em;
+        transition: all 1s;
+      }
+      
+      .ItemCarousel i.lni-plus:hover {
+        background: #ff0000;
+      }
+    `}</style>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Layout from "./components/Layout"
-import InicioCarousel from "./components/Carousel/Carousel"
+import Layout from "./components/Layout";
+import InicioCarousel from "./components/Carousel/Carousel";
+import ItemCarousel from "./components/ItemCarousel/ItemCarousel";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="entrada">
         <h2>Instituto Mexicano del Cemento y del Concreto A.C.</h2>
         <h3>Compartir el Conocimiento</h3>
+        <ItemCarousel/>
       </div>
     </Layout>
   )
