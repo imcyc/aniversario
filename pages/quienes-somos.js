f757ed34f353dbd4bf13b9ae382173c06c8d9458
axelobscura <axosar@gmail.com>
2021-11-02 20:04:19 -0600

    align

---
+++ pages/quienes-somos.js
@@ -0,0 +1,17 @@
+import Layout from "./components/Layout";
+
+export default function quienesSomos(){
+    return(
+        <Layout>
+            <div className="qsomos">
+                <div className="col ix">
+                    <h2>hola</h2>
+                </div>
+                <div className="col id">
+                    <h1>hola</h1>
+                </div>
+            </div>
+            
+        </Layout>
+    )
+}
