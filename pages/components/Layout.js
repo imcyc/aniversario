1baaa6d0b4b0d1ab993ae4c8fe1381ca2b5accd0
Axel <axosar@gmail.com>
2021-11-08 19:20:42 -0600

    qs

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -2,6 +2,13 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import { motion } from "framer-motion";
+
+const variants = {
+  hidden: { opacity: 0, x: -200, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: -100 },
+}
 
 export default function Layout({children}) {
   return (
@@ -15,7 +22,16 @@ export default function Layout({children}) {
 
       <Header />
 
-      {children}
+      <motion.main
+        variants={variants} // Pass the variant object into Framer Motion 
+        initial="hidden" // Set the initial state to variants.hidden
+        animate="enter" // Animated state to variants.enter
+        exit="exit" // Exit state (used later) to variants.exit
+        transition={{ type: 'linear' }} // Set the transition to linear
+        className=""
+      >
+        {children}
+      </motion.main>
 
       <Footer />
 
--- pages/quienes-somos.js
+++ pages/quienes-somos.js
@@ -1,17 +1,27 @@
 import Layout from "./components/Layout";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function quienesSomos(){
     return(
         <Layout>
-            <div className="qsomos">
-                <div className="col ix">
-                    <h2>QUIÉNES SOMOS</h2>
-                </div>
-                <div className="col id">
-                    <h1>hola</h1>
-                </div>
-            </div>
-            
+            <Container fluid={true} className="d-flex qsomos">
+                <Row style={{'width':'100vw'}} style={{'height':'85vh','display':'flex','justifyContent':'center','alignItems':'center'}}>
+                    <Col sm={2}>
+                        
+                    </Col>
+                    <Col>
+                        <h1>INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO A.C.</h1>
+                        <hr/>
+                        <p className="text-justify mr-5"><b>El IMCYC, A.C. es una asociación no lucrativa dedicada a la investigación, enseñanza y difusión de las técnicas de aplicación del cemento y del concreto</b>. Su misión es promover la utilización óptima del cemento y del concreto para satisfacer las necesidades del mercado con calidad, productividad y oportunidad, contribuyendo a mejorar el desempeño profesional, el desarrollo y beneficio económico de la industria, así como de la sociedad.<br/><br/>El IMCYC surgió en 1923 como un «Comité para Propagar el Uso del Cemento Portland». Ya <b>en 1959 se constituyó como Instituto Mexicano del Cemento y del Concreto, A.C., con el objetivo de investigar y promover métodos constructivos y soluciones técnicas que permitan un uso más económico y racional del concreto en la construcción</b>, para ofrecer el concreto al diseñador, al constructor y al propietario como la alternativa más ventajosa frente al empleo de otros materiales, además de crear más y mejores satisfactores para los asociados y sus clientes.<br/><br/>De este modo, se aportan nuevos conocimientos sobre el cemento y el concreto, y se enseña a trabajarlo con calidad, resolviendo los problemas de aplicación en las obras y orientando a los profesionales en sus múltiples oportunidades de utilización. Así mismo, se fortalecen nuestras estructuras, desarrollando al personal, ampliando nuestra presencia nacional e internacional y haciendo más efectivas y eficientes las operaciones. <b>El IMCYC es imagen, voz y representatividad del Sector Cemento y Concreto ante la sociedad, las autoridades nacionales y entidades internacionales, no estando comprometido con ninguna de las empresas del ramo</b>.</p>
+                    </Col>
+                </Row>
+            </Container>
+            <style jsx>{`
+                p {
+                    text-align: justify;
+                    margin-right: 50px;
+                }
+            `}</style>
         </Layout>
     )
 }
