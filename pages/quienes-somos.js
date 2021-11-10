0a5684994fb4ecaf357f9206af7a38690999491f
Axel <axosar@gmail.com>
2021-11-09 20:28:43 -0600

    video

--- pages/quienes-somos.js
+++ pages/quienes-somos.js
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { useState, VFC } from "react";
 import Layout from "./components/Layout";
 import Tab from 'react-bootstrap/Tab';
 import Nav from 'react-bootstrap/Nav';
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRight } from "react-bootstrap-icons";
 
+const VIDEO_WIDTH = 1920;
+const VIDEO_HEIGHT = 1080;
+
 export default function QuienesSomos() {
     const [categoria, setCategoria] = useState('first');
 
     return (
         <Layout>
-            <Container fluid={true} className={`d-flex qsomos ${categoria}`}>
+            <div className="video-background" style={{
+                'zIndex':'1'
+            }}>
+                <iframe
+                    width={VIDEO_WIDTH}
+                    height={VIDEO_HEIGHT}
+                    src="https://www.youtube.com/embed/D_vOqkEgmY0?autoplay=1&controls=0&mute=1&loop=1&modestbranding=1&showinfo=0&start=50&enablejsapi=1&&widgetid=3"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"/>
+            </div>
+            <Container fluid={true} className={`d-flex qsomos ${categoria}`} style={{
+                'zIndex':'100000'
+            }}>
                 <Row style={{
                     'width': '100vw', 
                     'height': '85vh', 
