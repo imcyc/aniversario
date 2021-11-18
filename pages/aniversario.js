2aba5ddce284b2d7f0dc4423ddd8de9bfe6ecb39
Axel <axosar@gmail.com>
2021-11-17 20:30:25 -0600

    after vid

--- pages/aniversario.js
+++ pages/aniversario.js
@@ -2,6 +2,7 @@ import Layout from "./components/Layout";
 import { Container } from "react-bootstrap";
 import Bkg from "./components/Bkg";
 import { motion } from "framer-motion";
+import { ArrowRight, ArrowDown, ArrowDownSquare } from 'react-bootstrap-icons';
 
 export default function aniversario(){
   return(
@@ -12,8 +13,8 @@ export default function aniversario(){
       >
         <div>
           <video id='background-video' autoPlay loop muted>
-            <source src='/videos/video1.mp4' type='/video/mp4' />
-            <source src='/videos/video1.mp4' type="video/ogg" /> 
+            <source src='/videos/video.mp4' type='/video/mp4' />
+            <source src='/videos/video.mp4' type="video/ogg" /> 
           </video>
         </div>
         <Container className="pagina-aniversario">
@@ -25,7 +26,8 @@ export default function aniversario(){
           </motion.div>
           <h1>25 AÑOS CONSTRUYENDO EXPERIENCIAS DIGITALES</h1>
           <hr/>
-          <p><b>El INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO A.C.</b><br/>es una asociación no lucrativa dedicada a la investigación, enseñanza y difusión de las técnicas de aplicación del cemento y del concreto. Su misión es <b>promover la utilización óptima del cemento y del concreto para satisfacer las necesidades del mercado con calidad, productividad y oportunidad</b>, contribuyendo a mejorar el desempeño profesional, el desarrollo y beneficio económico de la industria, así como de la sociedad.</p>
+          <p><b>El INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO A.C.</b><br/>es una asociación no lucrativa dedicada a la investigación, enseñanza y difusión de las técnicas de aplicación del cemento y del concreto. Su misión es <br/><b>promover la utilización óptima del cemento y del concreto para satisfacer las necesidades del mercado con calidad, productividad y oportunidad</b>,<br/> contribuyendo a mejorar el desempeño profesional, el desarrollo y beneficio económico de la industria, así como de la sociedad.</p>
+          <ArrowDownSquare size={30} color={'#fff'}/>
         </Container>
       </Bkg>
     </Layout>
