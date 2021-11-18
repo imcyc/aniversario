502ba36f656d646cac3404d012221e0425cead48
Axel <axosar@gmail.com>
2021-11-17 20:37:13 -0600

    gerencia vid

--- pages/gerencia-imcyc.js
+++ pages/gerencia-imcyc.js
@@ -5,6 +5,12 @@ export default function gerenciaImcyc(){
   return(
     <Layout>
       <Bkg imgx="/images/bkg_gerencia.jpg">
+      <div>
+          <video id='background-video' autoPlay loop muted>
+            <source src='/videos/video1.mp4' type='/video/mp4' />
+            <source src='/videos/video1.mp4' type="video/ogg" /> 
+          </video>
+        </div>
         <h1>GERENCIA IMCYC</h1>
       </Bkg>
     </Layout>
