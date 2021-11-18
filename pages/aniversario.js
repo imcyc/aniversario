d11a9bef9d518af172b818e407268b3e6a098114
Axel <axosar@gmail.com>
2021-11-17 19:40:18 -0600

    videos2

--- pages/aniversario.js
+++ pages/aniversario.js
@@ -1,6 +1,7 @@
 import Layout from "./components/Layout";
 import { Container } from "react-bootstrap";
 import Bkg from "./components/Bkg";
+import { motion } from "framer-motion";
 
 export default function aniversario(){
   return(
@@ -16,7 +17,12 @@ export default function aniversario(){
           </video>
         </div>
         <Container className="pagina-aniversario">
-          <img src="/images/logo_aniversario.png" alt="imcyc" title="imcyc" className="mb-5" style={{'width':'350px','opacity':'0.7'}} />
+          <motion.div
+            animate={{ opacity: [0, 1] }}
+            transition={{ ease: "easeIn", duration: 5 }}
+          >
+            <img src="/images/logo_aniversario.png" alt="imcyc" title="imcyc" className="mb-5" style={{'width':'350px','opacity':'0.7'}} />
+          </motion.div>
           <h1>25 AÑOS CONSTRUYENDO EXPERIENCIAS DIGITALES</h1>
           <hr/>
           <p><b>El INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO A.C.</b><br/>es una asociación no lucrativa dedicada a la investigación, enseñanza y difusión de las técnicas de aplicación del cemento y del concreto. Su misión es <b>promover la utilización óptima del cemento y del concreto para satisfacer las necesidades del mercado con calidad, productividad y oportunidad</b>, contribuyendo a mejorar el desempeño profesional, el desarrollo y beneficio económico de la industria, así como de la sociedad.</p>
