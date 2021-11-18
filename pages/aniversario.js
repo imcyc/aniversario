5f77b062d3957c84fe4a145dd3398ad2d0f0cbfd
Axel <axosar@gmail.com>
2021-11-17 20:35:33 -0600

    aniv entra

--- pages/aniversario.js
+++ pages/aniversario.js
@@ -27,7 +27,7 @@ export default function aniversario(){
           <h1>25 AÑOS CONSTRUYENDO EXPERIENCIAS DIGITALES</h1>
           <hr/>
           <p><b>El INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO A.C.</b><br/>es una asociación no lucrativa dedicada a la investigación, enseñanza y difusión de las técnicas de aplicación del cemento y del concreto. Su misión es <br/><b>promover la utilización óptima del cemento y del concreto para satisfacer las necesidades del mercado con calidad, productividad y oportunidad</b>,<br/> contribuyendo a mejorar el desempeño profesional, el desarrollo y beneficio económico de la industria, así como de la sociedad.</p>
-          <ArrowDownSquare size={30} color={'#fff'}/>
+          <ArrowDownSquare size={30} color={'#fff'} className="mt-3"/>
         </Container>
       </Bkg>
     </Layout>
