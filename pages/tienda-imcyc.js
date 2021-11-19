2da2d079b2cafd89e886dd71ea9608f995d39442
Axel <axosar@gmail.com>
2021-11-19 09:03:57 -0600

    ml

--- pages/tienda-imcyc.js
+++ pages/tienda-imcyc.js
@@ -1,11 +1,27 @@
 import Layout from "./components/Layout";
 import Bkg from "./components/Bkg";
 
+import { useEntries } from '../lib/swr-hooks';
+
 export default function tiendaImcyc(){
+  const { entries, isLoading } = useEntries();
+
+  if(isLoading){
+    return 'Cargando';
+  };
+
+  console.log(entries);
+
   return(
     <Layout>
       <Bkg imgx="/images/bkg_tienda.jpg">
         <h1>TIENDA IMCYC</h1>
+        <hr/>
+        {entries.map((producto) => (
+          <div className="d-flex flex-column">
+            <h2>{producto.name}</h2>
+          </div>
+        ))};
       </Bkg>
     </Layout>
   )
