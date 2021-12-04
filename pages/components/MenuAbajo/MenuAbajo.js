0cb245c2df82db9e7fcf92dac23a71fe1a8e7890
unknown <axosar@gmail.com>
2021-12-04 12:49:47 -0600

    menu

--- pages/components/MenuAbajo/MenuAbajo.js
+++ pages/components/MenuAbajo/MenuAbajo.js
@@ -12,51 +12,67 @@ export default function MenuAbajo() {
         <div className="menuiconos">
         <div className="MenuItem">
             <Link href="/quienes-somos">
-              <PersonCircle />
+              <a>
+                <PersonCircle />
+                <h2>QUIÉNES SOMOS</h2>
+              </a>
             </Link>
-            <h2>QUIÉNES SOMOS</h2>
           </div>
           <div className="MenuItem">
             <Link href="/gerencia-imcyc">
-              <Bricks />
+              <a>
+                <Bricks />
+                <h2>GERENCIA TÉCNICA</h2>
+              </a>
             </Link>
-            <h2>GERENCIA TÉCNICA</h2>
           </div>
           <div className="MenuItem">
             <Link href="/cursos">
-              <Building />
+              <a>
+                <Building />
+                <h2>CURSOS</h2>
+              </a>
             </Link>
-            <h2>CURSOS</h2>
           </div>
           <div className="MenuItem">
             <Link href="/directorio">
-              <Calendar2Event />
+              <a>
+                <Calendar2Event />
+                <h2>EVENTOS</h2>
+              </a>
             </Link>
-            <h2>EVENTOS</h2>
           </div>
           <div className="MenuItem">
             <Link href="/certificaciones">
-              <Journal />
+              <a>
+                <Journal />
+                <h2>CERTIFICACIONES</h2>
+              </a>
             </Link>
-            <h2>CERTIFICACIONES</h2>
           </div>
           <div className="MenuItem">
             <Link href="/membresias">
-              <PersonBoundingBox />
+              <a>
+                <PersonBoundingBox />
+                <h2>OCP</h2>
+              </a>
             </Link>
-            <h2>OCP</h2>
           </div>
           <div className="MenuItem">
             <Link href="/tienda-imcyc">
-              <Book />
+              <a>
+                <Book />
+                <h2>BIBLIOTECA</h2>
+              </a>
             </Link>
-            <h2>BIBLIOTECA</h2>
           </div>
           <div className="MenuItem">
             <Link href="/tienda-imcyc">
-              <Cart2 />
+              <a>
+                <Cart2 />
+                <h2>TIENDA</h2>
+              </a>
             </Link>
-            <h2>TIENDA</h2>
           </div>
         </div>
       </div>
