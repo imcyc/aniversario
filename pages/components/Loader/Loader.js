e134183ad15ca467404273fe0607602b5ef21439
unknown <axosar@gmail.com>
2021-12-23 20:38:38 -0600

    loading

--- pages/components/Loader/Loader.js
+++ pages/components/Loader/Loader.js
@@ -2,7 +2,7 @@ export default function Loader(){
   return(
     <div className="loader">
       <img src="/logo.svg" alt="" title="" style={{'width':'15em'}} />
-      <p className="mt-3">ESPERE UN MOMENTO POR FAVOR</p>
+      <p className="mt-3">CARGANDO...</p>
     </div>
   )
 }
