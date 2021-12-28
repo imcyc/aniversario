cfd73c7f727c666bfc663a4b4cc1b8d1f15346fa
Axel <axosar@gmail.com>
2021-12-28 16:20:48 -0600

    asesorias

--- pages/gerencia-tecnica/[tipo].js
+++ pages/gerencia-tecnica/[tipo].js
@@ -41,6 +41,62 @@ export default function Tipo(){
                     'norma': '(NMX-EC-17025-IMNC-2006)(METAL MECÁNICA)'
                 }
             ]
+        },
+        {
+            'categoria': 'cemento',
+            'personas': [
+                {
+                    'nombre': 'Ing. Mario Alberto Hernández Hernández',
+                    'puesto': 'Gerente Técnico',
+                    'mail': 'mhernandez@mail.imcyc.com',
+                    'telefono': '5276-7200 Ext.121'
+                },
+                {
+                    'nombre': 'Ing. Marco A. Gómez Solano',
+                    'puesto': 'Coordinador del Laboratorio de Cemento',
+                    'mail': 'mgomez@mail.imcyc.com',
+                    'telefono': '5276-7200 Ext.116'
+                }
+            ],
+            'acreditaciones': [
+                {
+                    'nombre': 'Entidad Mexicana de Acreditación, A.C. (EMA)',
+                    'acreditacion': 'No. C-053-039/11',
+                    'vigencia': 'Vigente a partir del: 2011-03-24',
+                    'norma': '(NMX-EC-17025-IMNC-2018)(CONSTRUCCIÓN)'
+                }
+            ]
+        },
+        {
+            'categoria': 'metrologia',
+            'personas': [
+                {
+                    'nombre': 'Ing. Vicente Gómez Bezares',
+                    'puesto': 'Jefe de Laboratorio de Calibración',
+                    'mail': 'vgomez@mail.imcyc.com',
+                    'telefono': '5276-7200 Ext.131'
+                }
+            ],
+            'acreditaciones': [
+                {
+                    'nombre': 'Entidad Mexicana de Acreditación, A.C. (EMA)',
+                    'acreditacion': 'No. C-053-039/11',
+                    'vigencia': 'Vigente a partir del: 2011-03-24',
+                    'norma': '(NMX-EC-17025-IMNC-2018)(CONSTRUCCIÓN)'
+                }
+            ]
+        },
+        {
+            'categoria': 'asesorias tecnicas',
+            'personas': [
+                {
+                    'nombre': 'Ing. Benito Pérez Alto',
+                    'puesto': 'Asistente de Asesorías Técnicas en Estructuras',
+                    'mail': 'bperez@mail.imcyc.com',
+                    'telefono': '(01 55) 5276 7200 Ext.110'
+                }
+            ],
+            'acreditaciones': []
         }
     ]
     const sectores = [
@@ -68,6 +124,54 @@ export default function Tipo(){
             'subcategoria': 'Pruebas no destructivas',
             'descripcion': 'Pruebas no destructivas.',
             'lista': ['"Determinación del índice de rebote utilizando el dispositivo conocido como esclerómetro (NMX-C-192-ONNCCE-2006)"','Determinación de la velocidad de pulso; método del ultrasonido (NMX-C-275-ONNCCE-2004)','Localización del acero de refuerzo en trabes, columnas y losas utilizando un detector de acero de refuerzo (Pachómetro)','Instrumentación de pruebas de carga en elementos estructurales horizontales (trabes y losas) de acuerdo al Capítulo XI Art. 239 y 240 del Reglamento de Construcciones del D.F.']
+        },
+        {
+            'categoria': 'cemento',
+            'subcategoria': 'Laboratorio de cemento',
+            'descripcion': 'Laboratorio de cemento.',
+            'lista': ['Análisis físico-químico de cemento. En conformidad con NMX C 414, ASTM C 150, C 595, C 1157 en vigor','Análisis físico-químico de cal hidratada. En conformidad con la Norma NMX C 005, ASTM C 141 en vigor','Análisis físico-químico de puzolanas. En conformidad con ASTM C 618 en vigor','Análisis químico de minerales no metálicos (arcilla, calizas, yesos, puzolanas etc). En conformidad con NMX C 131 y ASTM C 114 en vigor','Determinación de las propiedades físicas de morteros de cemento. En conformidad con NMX C 21 y ASTM C 91 en vigor','Análisis petrográficos de minerales no metálicos, rocas y agregados. En conformidad con ASTM C 295 en vigor','Análisis físico-químico de agua. En conformidad con NMX C 122 en vigor','Análisis físico-químico de yeso. En Conformidad con ASTM C 473 y C 472 en vigor','Análisis físico-químico de grout. En conformidad con CDR, en vigor','Análisis físico de adhesivos. En conformidad con NMX-C-420-ONNCCE, en vigor y con ANSI A 118., ANSI A 118. 4 Y ANSI A 118. 6','Determinación del calor de hidratación de cemento. En conformidad con NMX C 151 y ASTM C 186 en vigor','Determinación del contenido de cemento en muestras de concreto endurecido en conformidad con NMX C 154 y ASTM C 1084 en vigor','Determinación del índice de actividad de las adiciones en conformidad con NMX C 273 en vigor','Determinación de la retención de la humedad por medio de compuestos líquidos que forman membrana para el curado de concreto','En conformidad con ASTM C 156 y C 309 en vigor']
+        },
+        {
+            'categoria': 'metrologia',
+            'subcategoria': 'Calibración de máquinas uniaxiales',
+            'descripcion': 'Calibración de máquinas uniaxiales.',
+            'lista': ['Ensaye a compresión (cilindros, vigas y núcleos de concreto, cubos de mortero, prefabricados, etc)','Prueba de VRS y Marshall','Prueba Porter','Máquinas Universales','Máquinas de Ensaye de acero, cartón, etc.']
+        },
+        {
+            'categoria': 'metrologia',
+            'subcategoria': 'Calibración de dispositivos probadores de fuerza',
+            'descripcion': 'Calibración de dispositivos probadores de fuerza.',
+            'lista': ['Celdas de Carga','Anillos de Carga','Gatos Hidráulicos']
+        },
+        {
+            'categoria': 'metrologia',
+            'subcategoria': 'Mantenimiento preventivo',
+            'descripcion': 'El mantenimiento preventivo es una actividad programada de inspecciones, tanto de funcionamiento como de seguridad, ajustes, reparaciones, limpieza, lubricación, calibración, que deben llevarse a cabo en forma periódica en base a un plan establecido. El propósito es prever averías o desperfectos en su estado inicial y corregirlas para mantener la instalación en completa operación a los niveles y eficiencia óptimos.',
+            'lista': ['Limpieza general','Lubricación','Cambio de aceite','Ajuste','Reparaciones','Cambio de refacciones','Cumplimientos con normatividad']
+        },
+        {
+            'categoria': 'metrologia',
+            'subcategoria': 'Calibración de instrumentos para pesar de funcionamiento no automático de bajo alcance de medición',
+            'descripcion': 'Algunos aspectos importantes que se deben conocer son:',
+            'lista': ['La calibración nos permite determinar las desviaciones (errores) de indicación del instrumento para pesar con respecto al valor verdadero','Nuestro Alcance para la Calibración de Instrumentos para pesar es hasta 200 kg, estos instrumentos pueden ser: balanzas electrónicas, mecánicas, analíticas, microbalanzas, basculas de plataforma, etc. El servicio se realiza en el sitio donde se encuentre instalado el instrumento para pesar por ejemplo en Plantas de Concreto, Cemento, Laboratorios fijos, Laboratorios Móviles, Obras en desarrollo, Industrias Manufactureras, etc','Contamos con Juegos de pesas patrón Clase E2, F1 y M1','Se debe asegurar que el intervalo de calibración del instrumento para pesar cubra el intervalo de mediciones generalmente usado en las pruebas asegurando así la trazabilidad','Los periodos de calibración, son establecidos de acuerdo al sistema de confirmación metrológica, pero es importante considerar la calibración cuando se realizan reparaciones, mantenimiento, cuando se cambia de ubicación el instrumento para pesar o se tienen dudas en los resultados obtenidos']
+        },
+        {
+            'categoria': 'asesorias tecnicas',
+            'subcategoria': 'Asesorías técnicas en estructuras',
+            'descripcion': 'Antecedentes:',
+            'lista': ['La solución de los problemas técnicos complejos que se pueden presentar durante la ejecución o durante la vida útil de las obras civiles, así como los estudios de evaluación y/o rehabilitación de las estructuras requieren del apoyo de especialistas en diversos campos de la ingeniería civil','El dominio de las Estructuras es muy variado, amplio y complejo, para ello el IMCYC le ofrece los servicios de asesoría más completos, que puedan brindarle, un mejor apoyo dentro de este campo de la Ingeniería Civil','Los alcances cubren desde la estructura más simple como puede ser una viga, una losa o una columna hasta estructuras complejas como son: edificios de oficinas, residenciales, escolares, industriales y de estacionamientos; silos; torres de enfriamiento; tanques subterráneos, superficiales y elevados; puentes peatonales, carreteros y urbanos; naves y pisos industriales; así como túneles','Estos servicios pueden regirse en sus diferentes niveles de cálculo (análisis, diseño, revisión, evaluación) conforme a los Reglamentos vigentes en cada uno de los Estados de la República Mexicana, así como a otras normativas análogas: ACI 318, Normas AASHTO, Eurocódigos, UBC, etc','EL IMCYC ofrece para obras de concreto (armado o presforzado), mixtas (acero estructural-concreto), mampostería y acero estructural los siguientes servicios']
+        },
+        {
+            'categoria': 'asesorias tecnicas',
+            'subcategoria': '',
+            'descripcion': 'Servicios:',
+            'lista': ['DICTAMEN O INFORME TÉCNICO EN ESTABILIDAD Y SEGURIDAD ESTRUCTURAL','PERITAJES Y TERCERÍAS','DESARROLLO, INTERPRETACIÓN Y EVALUACIÓN DE PRUEBAS, DESTRUCTIVAS, SEMIDESTRUCTIVAS','ASESORÍAS Y CAPACITACIÓN EN TEMAS DE PATOLOGÍAS, REPRESENTACIÓN Y PRUEBAS','DICTAMEN O INFORME TÉCNICO EN DURABILIDAD DE CONSTRUCCIÓN','VISITAS DE INSPECCIÓN Y ASESORÍAS TÉCNICAS','SERVICIOS DE AUSCULTACIÓN DEL ACERO DE REFUERZO EN ELEMENTOS DE CONCRETO REFORZADO','REVISIÓN ESTRUCTURAL','ELABORACIÓN DE PROYECTOS ESTRUCTURALES','INSPECCIÓN Y LEVANTAMIENTO CUALITATIVO DE DAÑOS']
+        },
+        {
+            'categoria': 'asesorias tecnicas',
+            'subcategoria': '',
+            'descripcion': 'Nuestros clientes:',
+            'lista': ['']
         }
     ]
     return(
@@ -93,12 +197,12 @@ export default function Tipo(){
                             ))}
                         </Col>
                         <Col>
-                            {sectores.filter(tipo => tipo.categoria === titulo).map((tip, i) => (
-                                <div className="p-0 m-0" key={i}>
-                                    {/*<h4>{tip.categoria}</h4>*/}
-                                    <h2 style={{'textTransform':'uppercase'}}><ArrowRightCircle/> {tip.subcategoria}</h2>
+                            {sectores.filter(tipo => tipo.categoria == titulo).map((tip, i) => (
+                                <div className="p-0 m-0 labitems" key={i}>
+                                    <h2 style={{'textTransform':'uppercase'}}>{tip.subcategoria}</h2>
+                                    <hr/>
                                     <p className="text-justify">{tip.descripcion}</p>
-                                    <ul>
+                                    <ul className="pruebas">
                                         {tip.lista.map((lit, i) => (
                                             <li key={i}>{lit}</li>
                                         ))}
